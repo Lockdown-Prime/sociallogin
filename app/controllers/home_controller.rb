842a4bc22f696f18bf0a0db2e60b9e221e7a2702
JASON L. ANDERSON <jason.anderson@us.ibm.com>
2014-12-01 17:01:48 -0800

    Added Socal Auth code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  # def delete_all_data
+  #   User.delete_all
+  #   render text: 'Deleted data'
+  # end
+
+  # def show_all_data
+  #   str = ""
+  #   User.all.each do |u|
+  #     str += u.to_json
+  #   end
+  #   render text: str
+  # end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialAuth::Application.config.secret_key_base = '331780096a1183b3e8ab699707d5f2972c6caa7dde92943a309ed2b441e97b0d4c5feec1819fbf39218e5ea6917555d57c63cd652ec537ebbe8907bdd49fc1a3'
